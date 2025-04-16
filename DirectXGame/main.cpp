bf5898425b6ff199a3df4e98ab1fd09b25cab39a
YoshinoGento <k024g0013@g.neec.ac.jp>
2025-04-16 12:54:51 +0900

    a

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,41 @@
+#include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
+
+// これを書くことでKamataEmgine::と書かずに済む
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	// エンジンの初期化
+	KamataEngine::Initialize();
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ここに描画処理を記述する
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+	return 0;
+}
