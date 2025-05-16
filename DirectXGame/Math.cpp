ba82c2d6b91d33edf88b1905e0ee7d891ff54c12
YoshinoGento <k024g0013@g.neec.ac.jp>
2025-05-16 12:18:28 +0900

    a

--- DirectXGame/Math.cpp
+++ DirectXGame/Math.cpp
@@ -1,35 +1,36 @@
 #include "Math.h"
+#include "KamataEngine.h"
 
-Vector3& operator+=(Vector3& lhv, const Vector3& rhv) {
+KamataEngine::Vector3& operator+=(KamataEngine::Vector3& lhv, const KamataEngine::Vector3& rhv) {
 	lhv.x += rhv.x;
 	lhv.y += rhv.y;
 	lhv.z += rhv.z;
 	return lhv;
 }
 
-Vector3& operator-=(Vector3& lhv, const Vector3& rhv) {
+KamataEngine::Vector3& operator-=(KamataEngine::Vector3& lhv, const KamataEngine::Vector3& rhv) {
 	lhv.x -= rhv.x;
 	lhv.y -= rhv.y;
 	lhv.z -= rhv.z;
 	return lhv;
 }
 
-Vector3& operator*=(Vector3& v, float s) {
+KamataEngine::Vector3& operator*=(KamataEngine::Vector3& v, float s) {
 	v.x *= s;
 	v.y *= s;
 	v.z *= s;
 	return v;
 }
 
-Vector3& operator/=(Vector3& v, float s) {
+KamataEngine::Vector3& operator/=(KamataEngine::Vector3& v, float s) {
 	v.x /= s;
 	v.y /= s;
 	v.z /= s;
 	return v;
 }
 
-Matrix4x4& operator*=(Matrix4x4& lhm, const Matrix4x4& rhm) {
-	Matrix4x4 result{};
+KamataEngine::Matrix4x4& operator*=(KamataEngine::Matrix4x4& lhm, const KamataEngine::Matrix4x4& rhm) {
+	KamataEngine::Matrix4x4 result{};
 
 	for (size_t i = 0; i < 4; i++) {
 		for (size_t j = 0; j < 4; j++) {
@@ -42,79 +43,79 @@ Matrix4x4& operator*=(Matrix4x4& lhm, const Matrix4x4& rhm) {
 	return lhm;
 }
 
-Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2) {
-	Matrix4x4 result = m1;
+KamataEngine::Matrix4x4 operator*(const KamataEngine::Matrix4x4& m1, const KamataEngine::Matrix4x4& m2) {
+	KamataEngine::Matrix4x4 result = m1;
 
 	return result *= m2;
 }
 
 
 // 拡大縮小行列の作成
-Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+KamataEngine::Matrix4x4 MakeScaleMatrix(const KamataEngine::Vector3& scale) {
 
-	Matrix4x4 result{scale.x, 0.0f, 0.0f, 0.0f, 0.0f, scale.y, 0.0f, 0.0f, 0.0f, 0.0f, scale.z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+	KamataEngine::Matrix4x4 result{scale.x, 0.0f, 0.0f, 0.0f, 0.0f, scale.y, 0.0f, 0.0f, 0.0f, 0.0f, scale.z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 
 	return result;
 }
 
 // X軸回転行列の作成
-Matrix4x4 MakeRotateXMatrix(float theta) {
+KamataEngine::Matrix4x4 MakeRotateXMatrix(float theta) {
 	float sin = std::sin(theta);
 	float cos = std::cos(theta);
 
-	Matrix4x4 result{1.0f, 0.0f, 0.0f, 0.0f, 0.0f, cos, sin, 0.0f, 0.0f, -sin, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+	KamataEngine::Matrix4x4 result{1.0f, 0.0f, 0.0f, 0.0f, 0.0f, cos, sin, 0.0f, 0.0f, -sin, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 
 	return result;
 }
 
 // Y軸回転行列の作成
-Matrix4x4 MakeRotateYMatrix(float theta) {
+KamataEngine::Matrix4x4 MakeRotateYMatrix(float theta) {
 	float sin = std::sin(theta);
 	float cos = std::cos(theta);
 
-	Matrix4x4 result{cos, 0.0f, -sin, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, sin, 0.0f, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+	KamataEngine::Matrix4x4 result{cos, 0.0f, -sin, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, sin, 0.0f, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 
 	return result;
 }
 
 // Z軸回転行列の作成
-Matrix4x4 MakeRotateZMatrix(float theta) {
+KamataEngine::Matrix4x4 MakeRotateZMatrix(float theta) {
 	float sin = std::sin(theta);
 	float cos = std::cos(theta);
 
-	Matrix4x4 result{cos, sin, 0.0f, 0.0f, -sin, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+	KamataEngine::Matrix4x4 result{cos, sin, 0.0f, 0.0f, -sin, cos, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 
 	return result;
 }
 
 // 平行移動行列の作成
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
-	Matrix4x4 result{1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, translate.x, translate.y, translate.z, 1.0f};
+KamataEngine::Matrix4x4 MakeTranslateMatrix(const KamataEngine::Vector3& translate) {
+	KamataEngine::Matrix4x4 result{1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, translate.x, translate.y, translate.z, 1.0f};
 
 	return result;
 }
 
 // 拡大縮小・回転・平行移動行列を使ってアフィン変換行列を作る関数
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate) {
+KamataEngine::Matrix4x4 MakeAffineMatrix(const KamataEngine::Vector3& scale, const KamataEngine::Vector3& rot, const KamataEngine::Vector3& translate) {
 
 	// スケーリング行列の作成
-	Matrix4x4 matScale = MakeScaleMatrix(scale);
+	KamataEngine::Matrix4x4 matScale = MakeScaleMatrix(scale);
 
-	Matrix4x4 matRotX = MakeRotateXMatrix(rot.x);
-	Matrix4x4 matRotY = MakeRotateYMatrix(rot.y);
-	Matrix4x4 matRotZ = MakeRotateZMatrix(rot.z);
+	KamataEngine::Matrix4x4 matRotX = MakeRotateXMatrix(rot.x);
+	KamataEngine::Matrix4x4 matRotY = MakeRotateYMatrix(rot.y);
+	KamataEngine::Matrix4x4 matRotZ = MakeRotateZMatrix(rot.z);
 	// 回転行列の合成
-	Matrix4x4 matRot = matRotZ * matRotX * matRotY;
+	KamataEngine::Matrix4x4 matRot = matRotZ * matRotX * matRotY;
 
 	// 平行移動行列の作成
-	Matrix4x4 matTrans = MakeTranslateMatrix(translate);
+	KamataEngine::Matrix4x4 matTrans = MakeTranslateMatrix(translate);
 
 	// スケーリング、回転、平行移動の合成
-	Matrix4x4 matTransform = matScale * matRot * matTrans;
+	KamataEngine::Matrix4x4 matTransform = matScale * matRot * matTrans;
 
 	return matTransform;
 }
 
-KamataEngine::Vector3& Math::operator+=(KamataEngine::Vector3& lhs, const KamataEngine::Vector3& rhv) {
-	// TODO: return ステートメントをここに挿入します
-}
+//KamataEngine::Vector3& Math::operator+=(KamataEngine::Vector3& lhs, const KamataEngine::Vector3& rhv) {
+//	// TODO: return ステートメントをここに挿入します
+//}
--- DirectXGame/Math.h
+++ DirectXGame/Math.h
@@ -38,10 +38,10 @@ public:
   const float PI = 3.141592654f;  
 
   // 代入演算子オーバーロード  
-  KamataEngine::Vector3& operator+=(KamataEngine::Vector3& lhs, const KamataEngine::Vector3& rhv);  
+ /* KamataEngine::Vector3& operator+=(KamataEngine::Vector3& lhs, const KamataEngine::Vector3& rhv);  
   KamataEngine::Vector3& operator-=(KamataEngine::Vector3& lhs, const KamataEngine::Vector3& rhv);  
   KamataEngine::Vector3& operator*=(KamataEngine::Vector3& v, float s);  
-  KamataEngine::Vector3& operator/=(KamataEngine::Vector3& v, float s);  
+  KamataEngine::Vector3& operator/=(KamataEngine::Vector3& v, float s);  */
 
   // 単位行列の作成  
   KamataEngine::Matrix4x4 MakeIdentityMatrix();  
@@ -56,9 +56,9 @@ public:
   // アフィン変換行列の作成  
   KamataEngine::Matrix4x4 MakeAffineMatrix(const KamataEngine::Vector3& scale, const KamataEngine::Vector3& rot, const KamataEngine::Vector3& translate);  
 
-  // 代入演算子オーバーロード  
-  KamataEngine::Matrix4x4& operator*=(KamataEngine::Matrix4x4& lhm, const KamataEngine::Matrix4x4& rhm);  
+  //// 代入演算子オーバーロード  
+  //KamataEngine::Matrix4x4& operator*=(KamataEngine::Matrix4x4& lhm, const KamataEngine::Matrix4x4& rhm);  
 
-  // 2項演算子オーバーロード  
-  KamataEngine::Matrix4x4 operator*(const KamataEngine::Matrix4x4& m1, const KamataEngine::Matrix4x4& m2);  
+  //// 2項演算子オーバーロード  
+  //KamataEngine::Matrix4x4 operator*(const KamataEngine::Matrix4x4& m1, const KamataEngine::Matrix4x4& m2);  
 };
