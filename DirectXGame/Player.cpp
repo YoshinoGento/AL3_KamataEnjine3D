d7a19eaa504dcc0342ef959ffad25633d5ea4e14
YoshinoGento <k024g0013@g.neec.ac.jp>
2025-05-21 12:51:10 +0900

    9ページ

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -11,3 +11,16 @@ using namespace KamataEngine;
 void Player::Initialize(Model* model, Camera* camera, const Vector3& position) { worldTransform_.translation_ = position;
 	worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f;
 }
+
+void Player::Update() {
+	//移動入力
+
+
+	//移動
+	worldTransform_.translation_ += velocity_;
+
+	//行列更新
+
+
+
+}
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -7,8 +7,11 @@
 class Player {
 public:
 	void Initialize(Model* model, Camera* camera, const Vector3& position);
+	void Update();
 
 private:
 	// ワールドトランスフォーム
 	KamataEngine::WorldTransform worldTransform_;
+
+	Vector3 velocity_ = {};
 };
