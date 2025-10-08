b35a2f561c7d1ec5cd0cc5a61ce92ad795066c9b
YoshinoGento <k024g0013@g.neec.ac.jp>
2025-10-08 16:40:01 +0900

    a

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -2,9 +2,15 @@
 
 using namespace KamataEngine;
 
-void Player::Initialize() {
-	model_ = Model::Create(); //デフォルト立方体モデルとか
-	worldTransform_.Initialize(); // 位置、回転、スケールの初期化
+void Player::Initialize(Model* model, Camera* camera, const Vector3& position) {
+	assert(model);
+	model_ = model;
+	camera_ = camera;
+
+	// ワールド変換初期化
+	worldTransform_.Initialize();
+	worldTransform_.translation_ = position;
+	worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f; // Y軸回転で向きを調整
 
 
 
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -6,7 +6,7 @@ using namespace KamataEngine;
 class Player {
 public:
 	// 初期化
-	void Initialize();
+	void Initialize(Model * model,Camera *camera, const Vector3 &position);
 
 	// 更新
 	void Update();
@@ -16,8 +16,8 @@ public:
 
 private:
 
-	Model* model_ = nullptr; // モデルデータ
-
+	Model* model_ = nullptr;        // モデルデータ
+	Camera* camera_ = nullptr;      // カメラ
 	WorldTransform worldTransform_; // 位置・回転・スケール
 	
 };
