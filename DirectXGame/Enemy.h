d0acdd0bf3cdac94319c09a5696cf0382f8262c7
YoshinoGento <k024g0013@g.neec.ac.jp>
2025-11-17 12:01:05 +0900

    a

--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -1,16 +1,31 @@
 #pragma once
+#include "EnemyBullet.h"
 #include "KamataEngine.h"
+#include <list>
 
 using namespace KamataEngine;
 
 class Enemy {
 public:
 	// 初期化
-	void Initialize();
+	void Initialize(Model* model, Camera* camera, const Vector3& position);
 
 	// 更新
-	void Update();
+	void Update(Vector3 playerPos);
+
+	void Attack(const Vector3& playerPos);
 
 	// 描画
 	void Draw();
+
+	~Enemy();
+
+private:
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+	uint32_t textureHandle_ = 0u;
+	Camera* camera_ = nullptr;
+
+	std::vector<EnemyBullet*> bullets_; // 敵弾リスト
+	int attackTimer_ = 0;
 };
